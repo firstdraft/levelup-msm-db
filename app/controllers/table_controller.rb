9af7c59ba76bbdf2854126fa56f27387433140fe
Jelani Woods <jelani@firstdraft.com>
2023-02-16 14:02:06 -0600

    Support JSON requests

--- app/controllers/table_controller.rb
+++ app/controllers/table_controller.rb
@@ -2,20 +2,46 @@ class TableController < ApplicationController
   def movies
     @records = Movie.all.paginate(page: params[:page], per_page: 8)
     @table = Movie
+    set_columns
+    respond_to do |format|
+      format.html
+      format.json { render "index" }
+    end
   end
   
   def directors
     @records = Director.all.paginate(page: params[:page], per_page: 8)
     @table = Director
+    set_columns
+    respond_to do |format|
+      format.html
+      format.json { render "index" }
+    end
   end
   
   def actors
     @records = Actor.all.paginate(page: params[:page], per_page: 8)
     @table = Actor
+    set_columns
+    respond_to do |format|
+      format.html
+      format.json { render "index" }
+    end
   end
   
   def characters
     @records = Character.all.paginate(page: params[:page], per_page: 8)
     @table = Character
+    set_columns
+    respond_to do |format|
+      format.html
+      format.json { render "index" }
+    end
+  end
+
+  private
+
+  def set_columns
+    @columns = @table.columns.map(&:name)
   end
 end
